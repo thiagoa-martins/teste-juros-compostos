284f65c7b3ad81ecc783610f069f8d217ac9f141
Thiago <thiagoamartins0@gmail.com>
2022-06-14 16:38:03 -0300

    validacao do formulario

--- src/js/main.js
+++ src/js/main.js
@@ -6,20 +6,51 @@ const inputMonthlyPayment = document.forms["form"]["monthlyPayment"];
 const inputInterestRate = document.forms["form"]["interestRate"];
 const inputContributionTime = document.forms["form"]["contributionTime"];
 
+
+
 const structure = `{ "expr": "${inputMonthlyPayment.value} * (((1 + ${inputInterestRate.value}) ^ (${inputContributionTime.value * 12}) - 1) / 0.00517)" }`;
 
-let countComma = 0;
+let countCommaX = 0;
+let countCommaY = 0;
 
-inputInterestRate.addEventListener("keypress", function(e) {
+inputMonthlyPayment.addEventListener("keypress", function(e) {
 
     const keyCode = (e.keyCode ? e.keyCode : e.wich);
-    // 44 = "," 46 = "." Números = 48 ao 57 
+
     if((keyCode > 47 && keyCode < 58) || (keyCode == 44)) {
 
         if(keyCode == 44) {
     
-            if(countComma == 0) {
-                countComma += 1;
+            if(countCommaX == 0) {
+                countCommaX += 1;
+            } else {
+                e.preventDefault();
+            }    
+        }
+    } else {
+        e.preventDefault();
+    }
+
+    inputMonthlyPayment.addEventListener("keydown", function(e) {
+
+        const keyCode = (e.keyCode ? e.keyCode : e.wich);
+
+        if(keyCode == 8 && countCommaX == 1) {
+            countCommaX -= countCommaX;
+        }
+    });
+});
+
+inputInterestRate.addEventListener("keypress", function(e) {
+
+    const keyCode = (e.keyCode ? e.keyCode : e.wich);
+
+    if((keyCode > 47 && keyCode < 58) || (keyCode == 44)) {
+
+        if(keyCode == 44) {
+    
+            if(countCommaY == 0) {
+                countCommaY += 1;
             } else {
                 e.preventDefault();
             }    
@@ -29,16 +60,132 @@ inputInterestRate.addEventListener("keypress", function(e) {
     }
 
     inputInterestRate.addEventListener("keydown", function(e) {
+
         const keyCode = (e.keyCode ? e.keyCode : e.wich);
 
-        if(keyCode == 8) {
-
-            if(countComma == 1) {
-                countComma -= 1;
-            }
+        if(keyCode == 8 && countCommaY == 1) {
+            countCommaY -= countCommaY;
         }
     });
 });
 
+buttonSimulate.addEventListener("click", function() {
+
+    form.onsubmit = (e) => {
+
+        inputName.addEventListener("keyup", function() {
+            checkName();
+        });
 
 
+        inputMonthlyPayment.addEventListener("keyup", function() {
+            checkInputMonthlyPayment();
+        });
+
+
+        inputInterestRate.addEventListener("keyup", function() {
+            checkInputInterestRate();
+        });
+
+        inputContributionTime.addEventListener("keyup", function() {
+            checkInputContributionTime();
+        });
+
+        e.preventDefault();
+
+        let hasError = false;
+
+        function checkName() {
+
+            if(!inputName.value) {
+
+                hasError = true;
+    
+                inputName.classList.add("error");
+    
+                const label = inputName.nextSibling.nextSibling;
+        
+                label.innerText = "Informe o seu Nome Completo";
+        
+            } else {
+                inputName.classList.remove("error");
+    
+                const label = inputName.nextSibling.nextSibling;
+        
+                label.innerText = "";
+            }
+        }
+
+        function checkInputMonthlyPayment() {
+
+            if(!inputMonthlyPayment.value) {
+
+                hasError = true;
+    
+                inputMonthlyPayment.classList.add("error");
+    
+                const label = inputMonthlyPayment.nextSibling.nextSibling;
+        
+                label.innerText = "Informe a Mensalidade";
+        
+            } else {
+                inputMonthlyPayment.classList.remove("error");
+    
+                const label = inputMonthlyPayment.nextSibling.nextSibling;
+        
+                label.innerText = "";
+            }
+        }
+
+        function checkInputInterestRate() {
+
+            if(!inputInterestRate.value) {
+
+                hasError = true;
+
+                inputInterestRate.classList.add("error");
+
+                const label = inputInterestRate.nextSibling.nextSibling;
+        
+                label.innerText = "Informe a Taxa de Juros";
+        
+            } else {
+                inputInterestRate.classList.remove("error");
+
+                const label = inputInterestRate.nextSibling.nextSibling;
+        
+                label.innerText = "";
+            }
+        }
+
+        function checkInputContributionTime() {
+
+            if(!inputContributionTime.value) {
+
+                hasError = true;
+    
+                inputContributionTime.classList.add("error");
+    
+                const label = inputContributionTime.nextSibling.nextSibling;
+        
+                label.innerText = "Informe o Tempo de Contribuição";
+        
+            } else {
+                inputContributionTime.classList.remove("error");
+    
+                const label = inputContributionTime.nextSibling.nextSibling;
+        
+                label.innerText = "";
+            }
+        }
+
+        checkName();
+        checkInputMonthlyPayment();
+        checkInputInterestRate();
+        checkInputContributionTime();
+
+        if(!hasError) {
+            form.submit();
+        }
+    } 
+});
